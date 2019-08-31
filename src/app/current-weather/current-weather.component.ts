eb1d1dfd7d162de1e651500ebec4675d7e315e55
Hathawanian <mauihose2@gmail.com>
2019-09-01 10:12:37 +1200

    create dummy data and standardize
    * standardize code
    * add dummy data

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ICurrentWeather } from './interfaces'
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // NOTE: test data for the view
+    this.current = {
+      city: 'Wellington',
+      country: 'NZ',
+      date: 1567289511,
+      image: 'assets/images/sunny.svg',
+      temperature: 21,
+      description: 'sunny',
+    } as ICurrentWeather
   }
 
+  ngOnInit() {}
 }
