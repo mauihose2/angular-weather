4ef5fcd1e23682d1bacba697b3cdb42f2ca9d67f
Hathawanian <mauihose2@gmail.com>
2019-09-01 15:49:23 +1200

    implement http get
    * implement http get

--- src/app/current-weather/current-weather.service.ts
+++ src/app/current-weather/current-weather.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { ICurrentWeather } from './current-weather.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentWeatherService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  fetchWeather(city: string, country: string): Observable<ICurrentWeather> {
+    const url = `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`
+    return this.httpClient.get<ICurrentWeather>(url).pipe(
+      map(
+        apiData => {
+          const model : ICurrentWeather = {
+            city: apiData['name'],
+            country: apiData['sys']['country'],
+            date: apiData['dt'] * 1000,
+            image: `${environment.baseUrl}openweathermap.org/img/w/${apiData['weather'][0]['icon']}.png`,
+            tempCelsius: apiData['main']['temp'] - 273.15,
+            tempFahrenheit: apiData['main']['temp'] * 9 / 5 - 459.67,
+            description: apiData['weather'][0]['description']
+          }
+          return model;
+        }
+      )
+    )
+  }
 }
