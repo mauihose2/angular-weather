852903886e212a66c594cef0b4dea0320e6046f4
Hathawanian <mauihose2@gmail.com>
2019-09-01 12:37:21 +1200

    add service for the weather component
    * add service for the weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 import { ICurrentWeather } from './current-weather.interface'
+import { CurrentWeatherService } from './current-weather.service'
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.scss'],
+  providers: [CurrentWeatherService]
 })
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
